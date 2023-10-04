e7a13e475810def3656a64eef42be70aeb3da64f
Ali Ashraf <137727940+3li-Ashraf@users.noreply.github.com>
2023-10-05 01:46:44 +0300

    Add undo/redo feature

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const lightenModeButton = document.querySelector("#lightenModeButton");
 const gridSizeOutput = document.querySelector("#gridSize");
 const gridSizeSlider = document.querySelector("#gridSizeSlider");
 const gridLinesButton = document.querySelector("#gridLinesButton");
+const undoButton = document.querySelector("#undoButton");
+const redoButton = document.querySelector("#redoButton");
 const gridLinesStatusOutput = document.querySelector("#gridLinesStatus");
 const screenshotButton = document.querySelector("#screenshotButton");
 const clearButton = document.querySelector("#clearButton");
@@ -18,6 +20,8 @@ const grid = document.querySelector("#grid");
 
 const pixels = [];
 const usedPixels = [];
+const undoArray = [];
+const redoArray = [];
 let gridLinesStatus = "ON";
 let drawingMode = "pen";
 let drawingModeButton = penButton;
@@ -52,7 +56,9 @@ function constructGrid() {
 function destructGrid() {
     pixels.forEach(pixel => grid.removeChild(pixel));
     pixels.length = 0;
-    usedPixels.length = 0; 
+    usedPixels.length = 0;
+    undoArray.length = 0;
+    redoArray.length = 0;
 }
 
 function clearGrid() {
@@ -127,29 +133,37 @@ function toggleGridLines() {
 }
 
 function draw(pixel) {
+    redoArray.length = 0;
+    
    switch (drawingMode) {
     case "pen":
+        if (pixel.style.backgroundColor === penColorPicker.rgbValue && usedPixels.includes(pixel)) return;
+        undoArray.push(new Pixel(pixel, !usedPixels.includes(pixel) ? "blank" : "used"));
         pixel.style.backgroundColor = penColorPicker.value;
         usedPixels.add(pixel);
         break;
 
     case "eraser":
         if (!usedPixels.includes(pixel)) return;
+        undoArray.push(new Pixel(pixel, "erased"));
         pixel.style.backgroundColor = backgroundColorPicker.value;
         usedPixels.remove(pixel);
         break;
 
     case "colorful":
+        undoArray.push(new Pixel(pixel, !usedPixels.includes(pixel) ? "blank" : "used"));
         pixel.style.backgroundColor = getRandomColor();
         usedPixels.add(pixel);
         break;
 
     case "shading":
+        undoArray.push(new Pixel(pixel, !usedPixels.includes(pixel) ? "blank" : "used"));
         pixel.style.backgroundColor = shadeOrLightenColor(pixel.style.backgroundColor, "shade");
         usedPixels.add(pixel);
         break;
 
     case "lighten":
+        undoArray.push(new Pixel(pixel, !usedPixels.includes(pixel) ? "blank" : "used"));
         pixel.style.backgroundColor = shadeOrLightenColor(pixel.style.backgroundColor, "lighten");
         usedPixels.add(pixel);
         break;
@@ -175,6 +189,45 @@ function shadeOrLightenColor(color, option) {
     }
 }
 
+function undo() {
+    if (undoArray.length === 0) return;
+
+    const object = undoArray.pop();
+    redoArray.push(new Pixel(object.pixel, object.status));
+
+    switch (object.status) {
+        case "blank":
+            usedPixels.remove(object.pixel);
+            object.pixel.style.backgroundColor = backgroundColorPicker.value;
+            break;
+
+        case "used":
+        case "erased":
+            object.pixel.style.backgroundColor = object.color;
+            break;
+    }
+}
+
+function redo() {
+    if (redoArray.length === 0) return;
+
+    const object = redoArray.pop();
+    undoArray.push(new Pixel(object.pixel, object.status));
+
+    switch (object.status) {
+        case "used":
+        case "blank":
+            usedPixels.add(object.pixel);
+            object.pixel.style.backgroundColor = object.color;
+            break;
+
+        case "erased":
+            usedPixels.remove(object.pixel);
+            object.pixel.style.backgroundColor = backgroundColorPicker.value;
+            break;
+    }
+}
+
 function changeDrawingMode(newDrawingMode, newDrawingModeButton) {
     drawingMode = newDrawingMode;
     drawingModeButton.classList.remove("button-on");
@@ -184,17 +237,25 @@ function changeDrawingMode(newDrawingMode, newDrawingModeButton) {
 
 function screenshot() {
     html2canvas(grid).then(canvas => {
-        // Convert the canvas to an image data URL
         const screenshotDataUrl = canvas.toDataURL("image/png");
-
-        // Create a link to download the screenshot
         const downloadLink = document.createElement('a');
         downloadLink.href = screenshotDataUrl;
         downloadLink.download = "screenshot.png";
-
-        // Trigger the download
         downloadLink.click();
     });
+}
+
+function hex2rgb(hex) {
+    const R = parseInt(hex.slice(1, 3), 16);
+    const G = parseInt(hex.slice(3, 5), 16);
+    const B = parseInt(hex.slice(5, 7), 16);
+    return `rgb(${R}, ${G}, ${B})`;
+}
+
+function Pixel(pixel, status) {
+    this.pixel = pixel;
+    this.color = pixel.style.backgroundColor;
+    this.status = status ;
 }
 
 usedPixels.add = function(pixel) {
@@ -210,6 +271,10 @@ usedPixels.remove = function(pixel) {
 
 //---------------------------------------------------------------------------------------------------------------------
 //Event listeners
+
+penColorPicker.addEventListener("input", () => {
+    penColorPicker.rgbValue = hex2rgb(penColorPicker.value);
+});
 
 backgroundColorPicker.addEventListener("input", () => {
     const blankPixels = pixels.filter(pixel => !usedPixels.includes(pixel));
@@ -237,6 +302,24 @@ shadingModeButton.addEventListener("click", () => {
 
 lightenModeButton.addEventListener("click", () => {
     changeDrawingMode("lighten", lightenModeButton);
+});
+
+undoButton.addEventListener("click", () => {
+    undoButton.classList.add("button-on");
+    undo();
+});
+
+undoButton.addEventListener("transitionend", () => {
+    undoButton.classList.remove("button-on");
+});
+
+redoButton.addEventListener("click", () => {
+    redoButton.classList.add("button-on");
+    redo();
+});
+
+redoButton.addEventListener("transitionend", () => {
+    redoButton.classList.remove("button-on");
 });
 
 gridSizeSlider.addEventListener("input", () => {
@@ -270,6 +353,32 @@ window.addEventListener("load", () => {
     gridLinesStatusOutput.textContent = gridLinesStatus;
     if (gridLinesStatus === "ON") gridLinesButton.classList.add("button-on");
     gridSizeOutput.textContent = `${gridSizeSlider.value} X ${gridSizeSlider.value}`;
+    penColorPicker.rgbValue = hex2rgb(penColorPicker.value);
+});
+
+//-------------------------------------------------------------------------------------------------------
+// Simulate button click
+
+let isMouseDown = false;
+let buttonPressInterval;
+
+function simulateButtonClick(btn) {
+    btn.click();
+}
+
+undoButton.addEventListener('mousedown', () => {
+    isMouseDown = true;
+    buttonPressInterval = setInterval(() => simulateButtonClick(undoButton), 100);
+});
+
+redoButton.addEventListener('mousedown', () => {
+    isMouseDown = true;
+    buttonPressInterval = setInterval(() => simulateButtonClick(redoButton), 100);
+});
+
+window.addEventListener('mouseup', () => {
+    isMouseDown = false;
+    clearInterval(buttonPressInterval);
 });
 
 //---------------------------------------------------------------------------------------------------------------------
